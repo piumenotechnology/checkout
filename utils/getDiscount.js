265c67b6c31e7175191555fd746a799703f08217
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2025-05-21 23:42:05 +0800

    Add files via upload

--- utils/getDiscount.js
+++ utils/getDiscount.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getDiscount = async (token, eventId) => {
     const response = await axios.get(
-        `https://api.swoogo.com/api/v1/discounts?event_id=${eventId}`,
+        `https://api.swoogo.com/api/v1/discounts?event_id=${eventId}&fields=absolute_discount%2Capplicable_line_items%2Ccapacity%2Ccode%2Ccreated_at%2Ccustom_fees%2Cevent_id%2Cid%2Cnotes%2Cpercentage_discount%2Csold_out_message%2Ctype%2Cupdated_at`,
         {
             headers: {
                 'Authorization': `Bearer ${token}`,
