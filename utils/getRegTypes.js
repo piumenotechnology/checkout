9c7c288739bf865f25a33b4bcea441c835ff5216
Izzudin Fasya <izzudinfasya@gmail.com>
2025-06-05 00:16:46 +0800

    update

--- utils/getRegTypes.js
+++ utils/getRegTypes.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getRegTypes = async (token, eventId) => {
     const response = await axios.get(
-        `https://api.swoogo.com/api/v1/reg-types?event_id=${eventId}`,
+        `https://api.swoogo.com/api/v1/reg-types?event_id=${eventId}&fields=id%2Cname%2Cpublic_short_name%2Cadmin_short_name%2Cdescription%2Ccapacity%2Csold_out_message%2Cmin_group_size%2Cmax_group_size%2Ccreated_at%2Cupdated_at`,
         {
             headers: {
                 'Authorization': `Bearer ${token}`,
